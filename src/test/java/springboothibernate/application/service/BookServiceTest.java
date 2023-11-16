65d413b639782198f903a0605a512ed395a29490
Kim Long Ngo <ngokimlong2007@gmail.com>
2023-11-16 18:03:24 -0500

    Upgrade to SB3, JDK17 and JUnit5

--- src/test/java/springboothibernate/application/service/BookServiceTest.java
+++ src/test/java/springboothibernate/application/service/BookServiceTest.java
@@ -3,15 +3,15 @@ package springboothibernate.application.service;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import springboothibernate.application.model.Book;
 
 import java.util.List;
 
-@RunWith(SpringJUnit4ClassRunner.class)
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 public class BookServiceTest {
 
